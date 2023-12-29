20f760ffce3c90d56c77ce37383ea3cd78b9aa0b
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2023-12-29 22:22:23 +0530

    frontend

--- Front-end-todo/src/App.jsx
+++ Front-end-todo/src/App.jsx
@@ -1,16 +1,73 @@
-import "./App.css";
-import Headingh from "./Compoents/Hedingh";
-import TodoFrom from "./Compoents/TodoForm";
 
-function App() {
- 
 
-  return<center>
-          <Headingh></Headingh>
-          <TodoFrom></TodoFrom>
-        
-      </center>
-   
-}
+import React, { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import axios from "axios";
+import { baseURL } from "./utils/constant";
+import Popup from "./components/Popup";
+
+const App = () => {
+  const [toDos, setToDos] = useState([]);
+  const [input, setInput] = useState("");
+  const [updateUI, setUpdateUI] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupContent, setPopupContent] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/get`)
+      .then((res) => setToDos(res.data))
+      .catch((err) => console.log(err));
+  }, [updateUI]);
+
+  const saveToDo = () => {
+    axios
+      .post(`${baseURL}/save`, { toDo: input })
+      .then((res) => {
+        console.log(res.data);
+        setUpdateUI((prevState) => !prevState);
+        setInput("");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <main>
+      <div className="container">
+        <h1 className="title">ToDo App</h1>
+
+        <div className="input_holder">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Add a ToDo..."
+          />
+          <button onClick={saveToDo}>Add</button>
+        </div>
+
+        <div className="list">
+          {toDos.map((el) => (
+            <ToDo
+              key={el._id}
+              text={el.toDo}
+              id={el._id}
+              setUpdateUI={setUpdateUI}
+              setShowPopup={setShowPopup}
+              setPopupContent={setPopupContent}
+            />
+          ))}
+        </div>
+      </div>
+      {showPopup && (
+        <Popup
+          setShowPopup={setShowPopup}
+          popupContent={popupContent}
+          setUpdateUI={setUpdateUI}
+        />
+      )}
+    </main>
+  );
+};
 
 export default App;
--- Front-end-todo/src/Compoents/Hedingh.jsx
+++
@@ -1,8 +0,0 @@
-const Headingh = () => {
-  return (
-    <>
-      <h1>To-do-list </h1>
-    </>
-  );
-};
-export default Headingh;
